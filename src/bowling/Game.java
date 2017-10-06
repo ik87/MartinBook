97aaa2f268efe342021db410dbdf48ca923f3d19
worldown <d_dexter@mail.ru>
2017-10-06 11:23:45 +0300

    p124BeforeRefract

--- src/bowling/Game.java
+++ src/bowling/Game.java
@@ -4,27 +4,31 @@ public class Game {
 	private int itsScore = 0;
 	private int[] itsThrows = new int[21];
 	private int itsCurrentThrow = 0;
-	private int itsCurrentFrame = 0;
+	private int itsCurrentFrame = 1;
 	private boolean firstThrow = true;
 
 	public int score() {
-		return itsScore;
+		return scoreForFrame(getCurrentFrame() - 1);
 	}
 
 	public void add(int pins) {
 		itsThrows[itsCurrentThrow++] = pins;
 		itsScore += pins;
-		adjustCurrentFrame();
+		adjustCurrentFrame(pins);
 
 	}
 
-	private void adjustCurrentFrame() {
+	private void adjustCurrentFrame(int pins) {
 		if (firstThrow == true) {
-			firstThrow = false;
-			itsCurrentFrame++;
+			if(pins == 10)
+				itsCurrentFrame++;
+			else
+				firstThrow = false;
 		} else {
 			firstThrow = true;
+			itsCurrentFrame++;
 		}
+		itsCurrentFrame = Math.min(11, itsCurrentFrame);
 	}
 
 	public int scoreForFrame(int theFrame) {
@@ -32,12 +36,16 @@ public class Game {
 		int score = 0;
 		for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
 			int firstThrow = itsThrows[ball++];
+			if(firstThrow == 10) {
+				score += 10 + itsThrows[ball] + itsThrows[ball+1];
+			} else {
 			int secondThrow = itsThrows[ball++];
 			int frameScore = firstThrow + secondThrow;
 			if (frameScore == 10)
 				score += frameScore + itsThrows[ball];
 			else
 				score += frameScore;
+			}
 		}
 		return score;
 	}
--- src/unityTest/TestBowling.java
+++ src/unityTest/TestBowling.java
@@ -7,9 +7,9 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TestBowling {
-	private Game g  = new Game();
+	private Game g = new Game();
 
-	@Test
+	// @Test
 	public void testGameOneThrow() {
 
 		g.add(5);
@@ -18,12 +18,90 @@ public class TestBowling {
 	}
 
 	@Test
+	public void testSimpleStrike() {
+
+		g.add(10);
+		g.add(3);
+		g.add(6);
+		assertEquals(19, g.scoreForFrame(1));
+		assertEquals(28, g.score());
+		assertEquals(3, g.getCurrentFrame());
+	}
+
+	@Test
+	public void testPerfectGame() {
+
+		for (int i = 0; i < 12; i++) {
+			g.add(10);
+		}
+		assertEquals(300, g.score());
+		assertEquals(11, g.getCurrentFrame());
+	}
+
+	@Test
+	public void testEndOfArray() {
+
+		for (int i = 0; i < 9; i++) {
+			g.add(0);
+			g.add(0);
+		}
+		g.add(2);
+		g.add(8);
+		g.add(10);
+		assertEquals(20, g.score());
+	}
+
+	@Test
+	public void testSampleGame() {
+
+		g.add(1);
+		g.add(4);//5
+		g.add(4);
+		g.add(5);//14
+		g.add(6);
+		g.add(4);//29
+		g.add(5);
+		g.add(5);//49
+		g.add(10);//60
+		g.add(0);
+		g.add(1);//61
+		g.add(7);
+		g.add(3);//77
+		g.add(6);
+		g.add(4);//97
+		g.add(10);//116
+		g.add(2);
+		g.add(7);//125
+		g.add(6);//-
+		assertEquals(125, g.score());
+	
+	}
+	
+	@Test
+	public void testHeartBreak() {
+		for(int i = 0; i<11; i++)
+			g.add(10);
+			g.add(9);
+			assertEquals(299,g.score());
+	}
+	@Test
+	public void testTenthFrameSpare() {
+		for(int i=0; i<9; i++) 
+			g.add(10);
+			g.add(9);
+			g.add(1);
+			g.add(1);
+			assertEquals(270, g.score());
+		
+	}
+
+	@Test
 	public void testGameTwoThorwsNoMark() {
 
 		g.add(5);
 		g.add(4);
 		assertEquals(9, g.score());
-		assertEquals(1, g.getCurrentFrame());
+		assertEquals(2, g.getCurrentFrame());
 	}
 
 	@Test
@@ -36,7 +114,7 @@ public class TestBowling {
 		assertEquals(18, g.score());
 		assertEquals(9, g.scoreForFrame(1));
 		assertEquals(18, g.scoreForFrame(2));
-		assertEquals(2, g.getCurrentFrame());
+		assertEquals(3, g.getCurrentFrame());
 	}
 
 	@Test
@@ -46,16 +124,20 @@ public class TestBowling {
 		g.add(7);
 		g.add(3);
 		assertEquals(13, g.scoreForFrame(1));
+		assertEquals(2, g.getCurrentFrame());
 	}
+
 	@Test
 	public void testGameAfterSpare() {
-		
+
 		g.add(3);
 		g.add(7);
 		g.add(3);
 		g.add(2);
-		assertEquals(13, g.scoreForFrame(1)); 
-		assertEquals(18, g.score()); 
+		assertEquals(13, g.scoreForFrame(1));
+		assertEquals(18, g.scoreForFrame(2));
+		assertEquals(18, g.score());
+		assertEquals(3, g.getCurrentFrame());
 	}
 
 	@Test
